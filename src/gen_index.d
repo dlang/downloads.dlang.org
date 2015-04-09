43f5aaeb4dbdceb4a057757d25c642d5fc4961a8
Brad Roberts <braddr@puremagic.com>
2015-04-09 12:42:44 -0700

    work around changes in joiner behavior, update html to match dlang.org

--- src/gen_index.d
+++ src/gen_index.d
@@ -59,14 +59,14 @@ DirStructure makeIntoDirStructure(S3ListResults contents)
 string dirlinks(string[] dirnames)
 {
 
-    string result;
+    string result = "<a href=\"" ~ urlprefix ~ "/\">[root]</a>&nbsp;\n";
 
-    string accumulated;
+    string accumulated = "/";
 
     foreach (d; dirnames)
     {
         accumulated ~= d ~ "/";
-        result ~= `<a href="` ~ urlprefix ~ accumulated ~ `">` ~ (d.length ? d : "[root]") ~ `</a>&nbsp` ~ "\n";
+        result ~= `<a href="` ~ urlprefix ~ accumulated ~ `">` ~ (d.length ? d : "[root]") ~ `</a>&nbsp;` ~ "\n";
     }
 
     return result;
@@ -74,7 +74,10 @@ string dirlinks(string[] dirnames)
 
 void buildIndex(string[] dirnames, const DirStructure[string] dirs, const S3Object[string] files)
 {
-    string joined = join(dirnames, "/");
+    writefln("dirnames: %s", dirnames);
+    string joined = "/" ~ join(dirnames, "/");
+    joined = (joined != "/" ? (joined ~ "/") : "");
+
     writefln("generating index for: %s", joined);
 
     string dir = "/mnt/downloads.dlang.org/" ~ joined ~ "/";
@@ -91,7 +94,7 @@ void buildIndex(string[] dirnames, const DirStructure[string] dirs, const S3Obje
     {
         string filehtml;
 
-        filehtml ~= `<li><a href="` ~ urlprefix ~ joined ~ "/" ~ k ~ `/">` ~
+        filehtml ~= `<li><a href="` ~ urlprefix ~ joined ~ k ~ `/">` ~
             k ~ `</a></li>`;
 
         page ~= filehtml ~ "\n";
@@ -103,7 +106,7 @@ void buildIndex(string[] dirnames, const DirStructure[string] dirs, const S3Obje
     {
         string filehtml;
 
-        filehtml ~= `<li><a href="` ~ urlprefix ~ joined ~ "/" ~ k ~ `">` ~
+        filehtml ~= `<li><a href="` ~ urlprefix ~ joined ~ k ~ `">` ~
             k ~ `</a></li>`;
 
         page ~= filehtml ~ "\n";
@@ -118,7 +121,8 @@ void buildIndex(string[] dirnames, const DirStructure[string] dirs, const S3Obje
 
 void iterate(string[] dirnames, const ref DirStructure dir)
 {
-    dirnames ~= dir.name;
+    if (dir.name != "")
+        dirnames ~= dir.name;
 
     foreach (k; dir.subdirs.keys.sort)
         iterate(dirnames, dir.subdirs[k]);
@@ -152,163 +156,77 @@ void main()
 string genHeader()
 {
     return q"HEREDOC
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html lang="en-US">
-
 <!--
-Copyright (c) 1999-2013 by Digital Mars
-All Rights Reserved Written by Walter Bright
-http://digitalmars.com
+    Copyright (c) 1999-2015 by Digital Mars
+    All Rights Reserved Written by Walter Bright
+    http://digitalmars.com
   -->
-
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>D Programming Language</title>
+<meta charset="utf-8" />
+<meta name="keywords" content="D programming language" />
+<meta name="description" content="D Programming Language" />
+<title>Home - D Programming Language</title>
 <link rel="stylesheet" href="http://dlang.org/css/codemirror.css" />
-<link rel="stylesheet" type="text/css" href="http://dlang.org/css/style.css" />
-<link rel="stylesheet" type="text/css" href="http://dlang.org/css/print.css" media="print" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-<script src="http://dlang.org/js/codemirror.js"></script>
-<script src="http://dlang.org/js/run-main-website.js" type="text/javascript"></script>
-<script src="http://dlang.org/js/d.js"></script>
-<script src="http://dlang.org/js/run.js" type="text/javascript"></script>
-
+<link rel="stylesheet" href="http://dlang.org/css/style.css" />
+<link rel="stylesheet" href="http://dlang.org/css/print.css" media="print" />
+<link rel="stylesheet" href="http://dlang.org/css/cssmenu.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 <link rel="shortcut icon" href="http://dlang.org/favicon.ico" />
-
-<script src="http://dlang.org/js/hyphenate.js" type="text/javascript"></script>
-
-<script type="text/javascript">
-function bodyLoad()
-{
-var links = document.getElementById("navigation").getElementsByTagName("a");
-for (var i = 0; i < links.length; i++)
-{
-var url = "/" + links[i].getAttribute("href");
-if (window.location.href.match(url + "\x24") == url)
-{
-var cls = links[i].getAttribute("class");
-links[i].setAttribute("class", cls ? cls + " active" : "active");
-break;
-}
-}
-}
-</script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.1, maximum-scale=10.0" />
 </head>
-
-<body onLoad='bodyLoad()'>
-
-<div id="top">
-<div id="search-box">
-<form method="get" action="http://google.com/search">
-<img src="http://dlang.org/images/search-left.gif" width="11" height="22" /><input id="q" name="q" /><input type="image" id="search-submit" name="submit" src="http://dlang.org/images/search-button.gif" />
-<input type="hidden" id="domains" name="domains" value="dlang.org" />
-<input type="hidden" id="sourceid" name="sourceid" value="google-search" />
-<div id="search-dropdown">
-<select id="sitesearch" name="sitesearch" size="1">
-<option value="dlang.org">Entire D Site</option>
-<option value="dlang.org/phobos">Library Reference</option>
-<option value="digitalmars.com/d/archives">Newsgroup Archives</option>
-</select>
+<body id='Home' class='doc'>
+<script type="text/javascript">document.body.className += ' have-javascript';</script>
+<div id="top">  <div id="header">               <a class="logo" href="http://dlang.org/"><img id="logo" width="125" height="95" alt="D Logo" src="http://dlang.org/images/dlogo.svg"></a>
+                <span id="d-language-mobilehelper"><a href="http://dlang.org/" id="d-language">D Programming Language</a></span>
+        </div>
 </div>
-</form>
-</div>
-<div id="header">
-<a id="d-language" href="http://dlang.org/">
-<img id="logo" width="125" height="95" border="0" alt="D Logo" src="http://dlang.org/images/dlogo.png">
-D Programming Language</a>
-</div>
-</div>
-
-
-<div id="navigation">
-  
-
-<div class="navblock">
-<h2><a href="http://dlang.org/index.html" title="D Programming Language">D Home</a></h2>
-<ul><li><a href="http://dlang.org/overview.html" title="D language overview">Overview</a></li>
-        <li><a href="http://dlang.org/comparison.html" title="D feature list">Features</a></li>
-        <li><a href="http://dlang.org/download.html" title="Download a D compiler">Downloads &amp; Tools</a></li>
-        <li><a href="http://dlang.org/changelog.html" title="History of changes to D">Change Log</a></li>
-        <li><a href="http://dlang.org/bugstats.php" title="D issue and bug tracking system">Bug Tracker</a></li>
-        <li><a href="http://dlang.org/faq.html" title="Frequently Asked Questions">FAQ</a></li>
-        <li><a href="http://dlang.org/appendices.html">Appendices</a></li>
-        <li><a href="http://dlang.org/acknowledgements.html" title="Thank-you to these people who have helped with D">Acknowledgments</a></li>
-        <li><a href="http://dlang.org/sitemap.html" title="Documents on this site, indexed alphabetically">Sitemap</a></li>
-        <li><a href="http://digitalmars.com/d/1.0/index.html" title="D Programming Language 1.0">D1 Home</a></li>
-</ul>
+<div id="navigation">    <div id="search-box">        <form method="get" action="http://google.com/search">
+            <input type="hidden" id="domains" name="domains" value="dlang.org" />
+            <input type="hidden" id="sourceid" name="sourceid" value="google-search" />
+            <span id="search-query"><input id="q" name="q" placeholder="Search" /></span><span id="search-dropdown">                <select id="sitesearch" name="sitesearch" size="1">
+                    <option value="dlang.org">Entire D Site</option>
+                    <option  value="dlang.org/phobos">Library Reference</option>
+                    <option value="digitalmars.com/d/archives">Newsgroup Archives</option>
+                </select>
+            </span><span id="search-submit"><button type="submit"><i class="fa fa-search"></i><span>go</span></button></span>
+        </form>
     </div>
+    
+<div id="cssmenu"><ul>    <li><a href='http://dlang.org/'><span>D 2.067.0</span></a></li>
+    <li><a href='http://dlang.org/download.html'><span><b>Download</b></span></a></li>
+    <li><a href='http://dlang.org/getstarted.html'><span>Getting Started</span></a></li>
+    <li><a href='http://dlang.org/changelog.html'><span>Change Log</span></a></li>
+    <li class='has-sub'><a href='#'><span>D Reference</span></a>
+      <ul><li><a href='http://dlang.org/intro.html'>Introduction</a></li><li><a href='http://dlang.org/lex.html'>Lexical</a></li><li><a href='http://dlang.org/grammar.html'>Grammar</a></li><li><a href='http://dlang.org/module.html'>Modules</a></li><li><a href='http://dlang.org/declaration.html'>Declarations</a></li><li><a href='http://dlang.org/type.html'>Types</a></li><li><a href='http://dlang.org/property.html'>Properties</a></li><li><a href='http://dlang.org/attribute.html'>Attributes</a></li><li><a href='http://dlang.org/pragma.html'>Pragmas</a></li><li><a href='http://dlang.org/expression.html'>Expressions</a></li><li><a href='http://dlang.org/statement.html'>Statements</a></li><li><a href='http://dlang.org/arrays.html'>Arrays</a></li><li><a href='http://dlang.org/hash-map.html'>Associative Arrays</a></li><li><a href='http://dlang.org/struct.html'>Structs and Unions</a></li><li><a href='http://dlang.org/class.html'>Classes</a></li><li><a href='http://dlang.org/interface.html'>Interfaces</a></li><li><a href='http://dlang.org/enum.html'>Enums</a></li><li><a href='http://dlang.org/const3.html'>Const and Immutable</a></li><li><a href='http://dlang.org/function.html'>Functions</a></li><li><a href='http://dlang.org/operatoroverloading.html'>Operator Overloading</a></li><li><a href='http://dlang.org/template.html'>Templates</a></li><li><a href='http://dlang.org/template-mixin.html'>Template Mixins</a></li><li><a href='http://dlang.org/contracts.html'>Contract Programming</a></li><li><a href='http://dlang.org/version.html'>Conditional Compilation</a></li><li><a href='http://dlang.org/traits.html'>Traits</a></li><li><a href='http://dlang.org/errors.html'>Error Handling</a></li><li><a href='http://dlang.org/unittest.html'>Unit Tests</a></li><li><a href='http://dlang.org/garbage.html'>Garbage Collection</a></li><li><a href='http://dlang.org/float.html'>Floating Point</a></li><li><a href='http://dlang.org/iasm.html'>D x86 Inline Assembler</a></li><li><a href='http://dlang.org/ddoc.html'>Embedded Documentation</a></li><li><a href='http://dlang.org/interfaceToC.html'>Interfacing to C</a></li><li><a href='http://dlang.org/cpp_interface.html'>Interfacing to C++</a></li><li><a href='http://dlang.org/portability.html'>Portability Guide</a></li><li><a href='http://dlang.org/entity.html'>Named Character Entities</a></li><li><a href='http://dlang.org/memory-safe-d.html'>Memory Safety</a></li><li><a href='http://dlang.org/abi.html'>Application Binary Interface</a></li><li><a href='http://dlang.org/simd.html'>Vector Extensions
+      </a></li></ul>
+    <li><a href='https://dlang.org/phobos/index.html'><span>Standard library</span></a></li>
+    <li><a href='http://code.dlang.org'><span>More libraries</span></a></li>
+    <li class='has-sub'><a href='#'><span>Community</span></a>
+      <ul><li><a href='http://forum.dlang.org'>Forums</a></li><li><a href='irc://irc.freenode.net/d'>IRC</a></li><li><a href='http://github.com/D-Programming-Language'>D on GitHub</a></li><li><a href='http://wiki.dlang.org'>Wiki</a></li><li><a href='http://wiki.dlang.org/Review_Queue'>Review Queue</a></li><li><a href='http://twitter.com/search?q=%23dlang'>Twitter</a></li><li><a href='http://digitalmars.com/d/dlinks.html'>More Links
+      </a></li></ul>
+    <li class='has-sub'><a href='#'><span>Compilers &amp; Tools</span></a>
+      <ul><li><a href='http://dlang.org/dmd-windows.html'>dmd &ndash; reference compiler</a></li><li><a href='http://gdcproject.org'>gdc &ndash; gcc-based compiler</a></li><li><a href='http://wiki.dlang.org/LDC'>ldc &ndash; LLVM-based compiler</a></li><li><a href='http://dlang.org/rdmd.html'>rdmd &ndash; build tool</a></li><li><a href='http://dlang.org/htod.html'>htod &ndash; .h to .d
+      </a></li></ul>
+    <li class='has-sub'><a href='#'><span>Books &amp; Articles</span></a>
+      <ul><li><a href='http://ddili.org/ders/d.en/index.html'>Online Book (free)</a></li><li><a href='http://wiki.dlang.org/Books'>More Books</a></li><li><a href='http://dlang.org/howtos.html'>How-tos</a></li><li><a href='http://dlang.org/faq.html'>FAQ</a></li><li><a href='http://dlang.org/const-faq.html'>const(FAQ)</a></li><li><a href='http://dlang.org/comparison.html'>Feature Overview</a></li><li><a href='http://dlang.org/d-floating-point.html'>Floating Point</a></li><li><a href='http://dlang.org/wc.html'>Example: wc</a></li><li><a href='http://dlang.org/warnings.html'>Warnings</a></li><li><a href='http://dlang.org/rationale.html'>Rationale</a></li><li><a href='http://dlang.org/builtin.html'>Builtin Rationale</a></li><li><a href='http://dlang.org/ctod.html'>C to D</a></li><li><a href='http://dlang.org/cpptod.html'>C++ to D</a></li><li><a href='http://dlang.org/pretod.html'>C Preprocessor vs D</a></li><li><a href='http://dlang.org/code_coverage.html'>Code coverage analysis</a></li><li><a href='http://dlang.org/exception-safe.html'>Exception Safety</a></li><li><a href='http://dlang.org/hijack.html'>Hijacking</a></li><li><a href='http://dlang.org/intro-to-datetime.html'>Introduction to std.datetime</a></li><li><a href='http://dlang.org/lazy-evaluation.html'>Lazy Evaluation</a></li><li><a href='http://dlang.org/migrate-to-shared.html'>Migrating to Shared</a></li><li><a href='http://dlang.org/mixin.html'>Mixins</a></li><li><a href='http://dlang.org/regular-expression.html'>Regular Expressions</a></li><li><a href='http://dlang.org/safed.html'>SafeD</a></li><li><a href='http://dlang.org/templates-revisited.html'>Templates Revisited</a></li><li><a href='http://dlang.org/tuple.html'>Tuples</a></li><li><a href='http://dlang.org/variadic-function-templates.html'>Variadic Templates</a></li><li><a href='http://dlang.org/d-array-article.html'>D Slices
+      </a></li></ul>
+    <li class='has-sub'><a href='#'><span>Resources</span></a>
+      <ul><li><a href='http://dlang.org/library/index.html'>NEW Library Reference Preview</a></li><li><a href='http://dlang.org/bugstats.php'>Bug Tracker</a></li><li><a href='http://rainers.github.io/visuald/visuald/StartPage.html'>Visual D</a></li><li><a href='http://wiki.dlang.org/Editors'>Editors</a></li><li><a href='http://wiki.dlang.org/IDEs'>IDEs</a></li><li><a href='http://dlang.org/dstyle.html'>The D Style</a></li><li><a href='http://dlang.org/glossary.html'>Glossary</a></li><li><a href='http://dlang.org/acknowledgements.html'>Acknowledgments</a></li><li><a href='http://dlang.org/sitemap.html'>Sitemap
+      </a></li></ul>
+</ul></div>
+</div>
 
-<div class="navblock">
-<h2>Documentation</h2>
-<ul>   <li><a href="http://www.amazon.com/exec/obidos/ASIN/0321635361/classicempire">Book</a></li>
-        <li><a href="http://www.informit.com/articles/article.aspx?p=1381876">&nbsp;<font size=-1><span style="visibility: hidden">3</span>1.&nbsp;Tutorial</font></a></li>
-        <li><a href="http://www.informit.com/articles/article.aspx?p=1609144">&nbsp;<font size=-1>13.&nbsp;Concurrency</font></a></li>
-
-        <li><a href="http://dlang.org/language-reference.html">Language Reference</a></li>
-        <li><a href="http://dlang.org/phobos/index.html">Library Reference</a></li>
-        <li><a href="http://dlang.org/howtos.html" title="Helps for using D">How-tos</a></li>
-        <li><a href="http://dlang.org/articles.html">Articles</a></li>
-</ul>
+    <div id="news">      <div id="forum-summary">        <iframe src="http://forum.dlang.org/frame-announcements"></iframe>
+        <iframe src="http://forum.dlang.org/frame-discussions"></iframe>
+      </div>
+      <div id="twitter">        <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/D_Programming" data-widget-id="358057551562162176">Tweets by @D_Programming</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      </div>
     </div>
-
-<div class="navblock">
-<h2>Community</h2>
-<ul><li><a href="http://forum.dlang.org/" title="User forums">Forums</a></li>
-        <li><a href="http://github.com/D-Programming-Language" title="D on github">GitHub</a></li>
-        <li><a href="http://prowiki.org/wiki4d/wiki.cgi?FrontPage" title="Wiki for the D Programming Language">Wiki</a></li>
-        <li><a href="http://prowiki.org/wiki4d/wiki.cgi?ReviewQueue" title="Queue of current and upcoming standard library additions">Review Queue</a></li>
-        <li><a href="http://twitter.com/#search?q=%23d_lang" title="#d_lang on twitter.com">Twitter</a></li>
-        <li><a href="http://digitalmars.com/d/dlinks.html" title="External D related links">Links</a></li>
-        
-</ul>
-    </div>
-  
-<div id="translate" class="tool">Translate this page:
-        <div id="google_translate_element"></div><script type="text/javascript">
-        function googleTranslateElementInit() {
-          new google.translate.TranslateElement({
-            pageLanguage: 'en',
-            autoDisplay: false,
-            layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-          }, 'google_translate_element');
-        }
-        </script>
-<script type="text/javascript" src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-</div>
-</div>
-<div id="twitter">
-<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-<script>
-new TWTR.Widget({
-  version : 2,
-  type : 'profile',
-  rpp : 8,
-  interval : 30000,
-  width : 'auto',
-  height : 600,
-  theme : {
-    shell : {
-      background : '#1f252b',
-      //color : '#000000'
-    },
-    tweets : {
-      //background : '',
-      //color : '',
-      //links : ''
-    }
-  },
-  features : {
-    scrollbar : true,
-    loop : false,
-    live : true,
-    behavior : 'all'
-  }
-}).render().setUser('D_programming').start();
-</script>
-</div>
-
-<div id="content" style="margin-right:18em;" class='hyphenate'>
-
+<div class="hyphenate" id="content">    
+    
 HEREDOC";
 }
 
